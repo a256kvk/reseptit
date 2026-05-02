2eb682446250c722565ee404564a69e8c190488a
antero <>
2026-05-02 23:25:34 +0300

    added a python script for generating random user data for testing

---
+++ seed.py
@@ -0,0 +1,63 @@
+from os import system
+from random import choice, choices, randint
+from string import ascii_lowercase
+
+import db
+#import queries
+
+def randomword(length=5):
+    return "".join(choices(ascii_lowercase, k=length))
+
+def randomwords(n=5):
+    l=[]
+    for i in range(n):
+        l.append(randomword(randint(5,12)))
+    return l
+
+system("rm database.db")
+system("sqlite3 database.db <schema.sql")
+system("sqlite3 database.db <add_categories.sql")
+
+user_count = 10**4
+recipe_count = 10**5
+
+create_user_command = "INSERT INTO Users (username, password_hash) VALUES (?, ?)"
+
+create_recipe_command = """
+INSERT INTO Recipes (user_id, title, description, ingredients,
+                     instructions)
+VALUES (?, ?, ?, ?, ?)
+"""
+
+food_words = ["suomi", "banaani", "halloumi", "pata", "italia", "isoäidin"]
+ingredient_words = ["banaani", "mansikka", "jauheliha", "random", "suola",
+                    "muumio", "vetyperoksidi", "parmesaani", "juusto",
+                    "mozarella"]
+
+with db.get_cursor() as cur:
+    for i in range(1,user_count+1):
+        username=f"bot_user_{i}"
+        password=f"bot_password_{i}"
+        cur.execute(create_user_command, [username, password])
+
+    for i in range(1,recipe_count+1):
+        user_id = randint(1, user_count)
+
+        title = " ".join([choice(food_words), randomword(),
+                          choice(ingredient_words)])
+
+        description = " ".join(randomwords(randint(1, 10))
+                               + choices(food_words, k=5)
+                               + choices(ingredient_words, k=5))
+
+        ingredients = ", ".join(randomwords(randint(0, 1))
+                               + choices(food_words, k=randint(0, 2))
+                               + choices(ingredient_words, k=randint(0, 6)))
+
+        instructions = " ".join(randomwords(randint(0, 50))
+                               + choices(food_words, k=randint(0, 20))
+                               + choices(ingredient_words, k=randint(0, 60)))
+
+        params = [user_id, title, description, ingredients, instructions]
+
+        cur.execute(create_recipe_command,params)
